413fcc122830535636ac5fd047a7a5abad608fff
jeyofdev <jgregoire.pro@gmail.com>
2022-04-15 12:08:46 +0200

    feat : init apollo-server-express with graphql

--- dist/index.js
+++ dist/index.js
@@ -1,5 +1,43 @@
-const calcul = (a, b) => {
-    return a + b;
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
 };
-console.log(calcul(1, 2));
+Object.defineProperty(exports, "__esModule", { value: true });
+const apollo_server_express_1 = require("apollo-server-express");
+const apollo_server_core_1 = require("apollo-server-core");
+const Express = require("express");
+const http_1 = require("http");
+require("dotenv/config");
+const { PORT } = process.env;
+const schema = (0, apollo_server_core_1.gql)(`
+    type Query {
+        name: String
+    }
+`);
+const resolvers = {
+    Query: {
+        name: () => 'John Doe',
+    },
+};
+const main = () => __awaiter(void 0, void 0, void 0, function* () {
+    const app = Express();
+    const httpServer = (0, http_1.createServer)(app);
+    const server = new apollo_server_express_1.ApolloServer({
+        typeDefs: schema,
+        resolvers,
+        plugins: [(0, apollo_server_core_1.ApolloServerPluginDrainHttpServer)({ httpServer })],
+        context: {},
+    });
+    yield server.start();
+    server.applyMiddleware({ app, path: '/' });
+    yield new Promise((resolve) => httpServer.listen({ port: PORT }, resolve));
+    console.log(`🚀 Server ready at http://localhost:${PORT}${server.graphqlPath}`);
+});
+main();
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,49 @@
-const calcul = (a: number, b: number) => a + b;
-console.log(calcul(1, 2));
+/* eslint-disable no-promise-executor-return */
+import { ApolloServer } from 'apollo-server-express';
+import { ApolloServerPluginDrainHttpServer, gql } from 'apollo-server-core';
+import * as Express from 'express';
+import { createServer } from 'http';
+import 'dotenv/config';
 
-console.log('hello');
+const { PORT } = process.env;
+
+// Set schema
+const schema = gql(`
+    type Query {
+        name: String
+    }
+`);
+
+const resolvers = {
+  Query: {
+    name: () => 'John Doe',
+  },
+};
+
+const main = async () => {
+  // Create server with express
+  const app = Express();
+  const httpServer = createServer(app);
+
+  // Init apollo server
+  const server = new ApolloServer({
+    typeDefs: schema,
+    resolvers,
+    plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+    context: {},
+  });
+
+  await server.start();
+  server.applyMiddleware({ app, path: '/' });
+
+  // run a web server
+  await new Promise<void>((resolve) =>
+    httpServer.listen({ port: PORT }, resolve)
+  );
+
+  console.log(
+    `🚀 Server ready at http://localhost:${PORT}${server.graphqlPath}`
+  );
+};
+
+main();
